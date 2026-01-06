571223ebdc456af7bb18f5a8cf00eefadf5020be
Victor Dallagnol Bento <victodallbento@hotmail.com>
2026-01-05 22:01:15 -0300

    add tables and relationships

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import fastify from "fastify";
+import { PrismaClient } from "./generated/prisma/client";
 
 export const app = fastify();
+
+const prisma = new PrismaClient({
+  accelerateUrl: process.env.DATABASE_URL!, // Non-null assertion
+  omit: {
+    user: {
+      password: true,
+    },
+  },
+});
+
+async function createUser() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Victor D Bento",
+      email: "victor@test.com",
+    },
+  });
+  console.log("User created:", user);
+}
+
+createUser();
